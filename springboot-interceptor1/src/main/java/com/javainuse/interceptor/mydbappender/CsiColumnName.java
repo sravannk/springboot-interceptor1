f911d2c28b50f450f8678d7cab390aa9fff6d51b
shravan <sravann.k@outlook.com>
2018-07-08 17:10:11 +0530

    commit

---
+++ springboot-interceptor1/src/main/java/com/javainuse/interceptor/mydbappender/CsiColumnName.java
@@ -0,0 +1,15 @@
+package com.javainuse.interceptor.mydbappender;
+
+
+
+public enum CsiColumnName {
+
+	EVENT_ID,
+    requestid, 
+    uri, 
+    uritype, 
+    operation, 
+    status, 
+    servicetype, 
+    parent
+}
